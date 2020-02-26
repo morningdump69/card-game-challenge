5356e8e63e83ae3577a214efe45091fe0a25b482
rossburrows <rossburrows96@gmail.com>
2020-02-26 16:28:03 +0000

    Updates

---
+++ precache-manifest.67ecee7450d2895e743fb0a93f060a1b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71e497ff8ed6992b4b91ce0cbe2de60",
+    "url": "/card-game-challenge/index.html"
+  },
+  {
+    "revision": "cd25f9be82965c1c4481",
+    "url": "/card-game-challenge/static/css/main.3454805e.chunk.css"
+  },
+  {
+    "revision": "b0c0b814ad51f020a365",
+    "url": "/card-game-challenge/static/js/2.a1121a35.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/card-game-challenge/static/js/2.a1121a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd25f9be82965c1c4481",
+    "url": "/card-game-challenge/static/js/main.404ec31f.chunk.js"
+  },
+  {
+    "revision": "0373758f822ccb560c68",
+    "url": "/card-game-challenge/static/js/runtime-main.13214409.js"
+  },
+  {
+    "revision": "2c593025292055d6b34711d9eb2ed406",
+    "url": "/card-game-challenge/static/media/DryBowserSmash3.2c593025.png"
+  },
+  {
+    "revision": "511f5f28dd015de336d315be51981b07",
+    "url": "/card-game-challenge/static/media/Morton_SSBU.511f5f28.png"
+  },
+  {
+    "revision": "5e22ba9dd6b4c755c43d01e75c448494",
+    "url": "/card-game-challenge/static/media/Wario_MP100.5e22ba9d.png"
+  },
+  {
+    "revision": "2a72b74a74258d99a08964d2bc908853",
+    "url": "/card-game-challenge/static/media/babymario.2a72b74a.jpg"
+  },
+  {
+    "revision": "69f44bd2e7c7279b1925d677e673c54f",
+    "url": "/card-game-challenge/static/media/bowser.69f44bd2.jpg"
+  },
+  {
+    "revision": "ed560b7c88998e45fbf0c2bd264b4276",
+    "url": "/card-game-challenge/static/media/roy-kooper.ed560b7c.png"
+  },
+  {
+    "revision": "5ba830fa161031726a0398d94b1eb909",
+    "url": "/card-game-challenge/static/media/waluigi.5ba830fa.jpg"
+  },
+  {
+    "revision": "64e02a7632a2bea232f646452a0fea57",
+    "url": "/card-game-challenge/static/media/yoshi.64e02a76.png"
+  }
+]);
